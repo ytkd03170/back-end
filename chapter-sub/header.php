edb4d525eb13a5a28465709180777a083c27e733
ytkd03170 <90828468+ytkd03170@users.noreply.github.com>
2021-11-11 18:41:24 +0900

    Add files via upload

---
+++ chapter-sub/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP Sample Programs</title>
+
+<link rel="stylesheet" href="../style.css">
+
+</head>  <!-- ここまでがページの仕様 映らない-->
+<body> <!--ここから下が映る-->
+
