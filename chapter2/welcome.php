2efa7e205229017520742c4ab18064e550e7b35b
ytkd03170 <90828468+ytkd03170@users.noreply.github.com>
2021-11-11 18:58:38 +0900

    Add files via upload

---
+++ chapter2/welcome.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<br />
+html ver4 の書き方 
+
+<meta charset="UTF-8">
+<br>
+html ver5 の書き方 
+<h2><?php	echo 'こんにちは';?></h2>
+	echo するだけならこれでいい
+<h3><?=	'こんにちは';?></h3>
+		本の56Pから作ってください
